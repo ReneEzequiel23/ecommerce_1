4f46ca34885dff9bb606629b98a18007706b107e
ReneEzequiel23 <150867485+ReneEzequiel23@users.noreply.github.com>
2026-05-21 18:28:47 -0700

    Filtro de catalogo
    se implementó la lógica del filtro en catalogoCliente.js

--- ecommerce_WEB/src/main/webapp/javascript/catalogoCliente.js
+++ ecommerce_WEB/src/main/webapp/javascript/catalogoCliente.js
@@ -5,8 +5,11 @@
 
 const API_PRODUCTOS = '/ecommerce_WEB/api/productos';
 
+let catalogoOriginal = [];
+
 document.addEventListener('DOMContentLoaded', () => {
     cargarCatalogo();
+    configurarFiltros();
 });
 
 /**
@@ -17,8 +20,8 @@ async function cargarCatalogo() {
         const respuesta = await fetch(API_PRODUCTOS);
 
         if (respuesta.ok) {
-            const listaProductos = await respuesta.json();
-            dibujarTabla(listaProductos);
+            catalogoOriginal = await respuesta.json();
+            dibujarTabla(catalogoOriginal);
         } else {
             document.getElementById('cuerpoCatalogoCliente').innerHTML =
                     '<tr><td colspan="4" style="text-align:center;">No hay libros disponibles en este momento.</td></tr>';
@@ -93,4 +96,43 @@ function verDetalles(id) {
 
 function dejarResena(id) {
     window.location.href = `crearResena.jsp?productoId=${id}`;
+}
+
+function configurarFiltros() {
+    const formulario = document.getElementById('formFiltros');
+    
+    formulario.addEventListener('submit', (evento) => {
+        // Evitamos que la página parpadee y recargue
+        evento.preventDefault();
+        
+        // 1. Obtenemos lo que el usuario escribió
+        const textoBusqueda = document.getElementById('f-nombre').value.toLowerCase();
+        const usarPrecio = document.getElementById('check-precio').checked;
+        const precioLimite = parseFloat(document.getElementById('f-precio').value);
+        const rango = document.querySelector('input[name="rango"]:checked').value; // 'mayor' o 'menor'
+        
+        // 2. Empezamos con la lista completa
+        let listaFiltrada = catalogoOriginal;
+        
+        // 3. Filtro por nombre (Si escribió algo)
+        if (textoBusqueda !== "") {
+            listaFiltrada = listaFiltrada.filter(libro => 
+                libro.nombre.toLowerCase().includes(textoBusqueda)
+            );
+        }
+        
+        // 4. Filtro por precio (Solo si activó la palomita y escribió un número válido)
+        if (usarPrecio && !isNaN(precioLimite)) {
+            if (rango === 'mayor') {
+                // Traer los que cuesten IGUAL O MÁS que el límite
+                listaFiltrada = listaFiltrada.filter(libro => libro.precio >= precioLimite);
+            } else if (rango === 'menor') {
+                // Traer los que cuesten IGUAL O MENOS que el límite
+                listaFiltrada = listaFiltrada.filter(libro => libro.precio <= precioLimite);
+            }
+        }
+        
+        // 5. Borramos la tabla actual y la volvemos a dibujar solo con los que pasaron el filtro
+        dibujarTabla(listaFiltrada);
+    });
 }
